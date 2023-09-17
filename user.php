d77e9e442f76ab89c3c39cebcbafd1da987330d2
Ispraggins1 <143848081+Ispraggins1@users.noreply.github.com>
2023-09-17 16:16:18 -0400

    Small update to the header and moving code from home.html to here

--- user.php
+++ user.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 ?>
 
 <!DOCTYPE html>
-
+<html lang="en">
 <html>
     <link rel="stylesheet" href="dashstyle.css">
     <div class="header">
@@ -17,7 +17,9 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     </div>
 
 <head>
-    <title>User Dashboard</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>User Dashboard</title>
 </head>
 <body>
 
