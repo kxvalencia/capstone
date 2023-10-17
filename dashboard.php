5219fc1ccc824d7ba3acbbd717e03e0a1c3e6175
ItCapstoneGroup4 <145810932+ItCapstoneGroup4@users.noreply.github.com>
2023-10-17 12:32:46 -0400

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -12,13 +12,28 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
 <html>
     
-    <link rel="stylesheet" href="dashstyle.css">
+    <link rel="stylesheet" href="dashboardTest.css">
     
     <body>
     
     <div class="header" id="head">
-        <h1><a href="create_patient_page.php">Create Page</a> <a href="https://www.tacobell.com/">Notice</a> <span style="float:right;"><a href="index.html">Logout</a> Nurse Info</span></h1>
-    </div>
+        <!--<h1><a href="create_patient_page.php">Create Page</a> <a href="https://www.tacobell.com/">Notice</a> <span style="float:right;"><a href="index.html">Logout</a> Nurse Info</span></h1>-->
+        <div class="profile">
+        <a href="#"><img src="ProfilePic.png" alt=""/></a>
+      
+      </div>
+      <h1>Dashboard</h1>
+      <div class="nav">
+        <a href="patient.php">Patients</a>
+        <a href="prescriptions.php">Prescriptions</a>
+        <a href="appointments.php">Appointments</a>
+        <a href="reports.php">Reports & Analytics</a>
+
+      </div>
+      </div>
+
+      
+
 
     <script>
         window.onscroll = function() {scrollf()};
@@ -49,13 +64,61 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <title>User Dashboard</title>
 </head>
 <body>
-
-<h1>Welcome <?php echo $_SESSION['name']; ?>!</h1>
-<p>email: <?php echo $_SESSION['email']; ?></p>
+<!-- Moved into 'Nav1 div'
+</p><h1>Welcome <?php echo $_SESSION['name']; ?>!</h1>
+<p>email: <?php echo $_SESSION['email']; ?>
 
 <form action="logout.php" method="post">
     <input type="submit" value="Logout">
 </form>
+      -->  
+<div class="wrapper">               
+  <nav class="nav1">
+  </p><h1>Welcome <?php echo $_SESSION['name']; ?>!</h1>
+  <p>email: <?php echo $_SESSION['email']; ?>                       
+      <div class="logo">                      
+        <img src="logo.png" alt="">
+        <span>Valencia Medical</span>
+      </div>                               
+      
+      <a href="#">Dashboard</a>
+      <a href="#">Example 1</a>
+      <a href="#">Example 2</a>
+      <a href="#">Example 3</a>
+      <a href="#">Example 4</a>
+
+
+    <form action="logout.php" class="logout" method="post">
+    <input type="submit" value="Logout">
+    </form>
+      <a href="" class="logout">Logout</a>
+    </div>                                  
+  </nav>                                     
+  <div class="mainBody">             
+    <div class="header1">
+    <span></span>
+    </div>
+    <div class="followingWrapper">
+  <div class="card">
+    <img src="AppointmentsPic.png" alt=""/>
+    <h3>7 Patients</h3>
+  </div>
+  <div class="card">
+    <img src="PatientPic.png" alt=""/>
+    <h3>5 Upcoming Appointments</h3>
+  </div>
+  
+  <div class="card">
+  <img src="HospitalPic.png" alt=""/>
+    <h3>Insert Widget</h3>
+  </div>
+  
+  <div class="card">
+  <img src="RandomPic.png" alt=""/>
+    <h3>Insert Widget</h3>
+  </div>
+</div>
+</div>
 
 <!-- You can add more HTML content here, and if needed, embed more PHP wherever necessary. -->
 
