3ff78da6c2392f196af6c47bb7091ba7a10d0fea
Ispraggins1 <143848081+Ispraggins1@users.noreply.github.com>
2023-11-11 18:50:47 -0500

    Create reports.php

---
+++ reports.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Valencia Medical - Reports & Analytics</title>
+  <link rel="stylesheet" href="reports.css">
+</head>
+<body>
+
+<div class="header" id="head">
+  <div class="profile">
+    <a href="#"><img src="./img/ProfilePic.png" alt="Profile Picture"/></a>
+    <h1>Valencia Medical</h1>
+  </div>
+  <div class="nav">
+    <a href="patient.php">Patients</a>
+    <a href="prescriptions.php">Prescriptions</a>
+    <a href="appointments.php">Appointments</a>
+    <a href="reports.php">Reports & Analytics</a>
+  </div>
+</div>
+
+<div class="mainBody">
+  <!-- Reports & Analytics Dashboard Overview -->
+  <div class="dashboard-overview">
+    <h2>Dashboard Overview</h2>
+    <div class="kpi-container">
+      <div class="kpi">Patient Wait Times: <span id="patient-wait-times">15 mins</span></div>
+      <div class="kpi">Bed Occupancy Rate: <span id="bed-occupancy-rate">75%</span></div>
+      <div class="kpi">Average Treatment Costs: <span id="average-treatment-costs">$1,200</span></div>
+    </div>
+  </div>
+
+  <!-- Patient Reports Section -->
+  <div class="patient-reports-section">
+    <h2>Patient Reports</h2>
+    <!-- Content such as tables and charts -->
+  </div>
+
+  <!-- Financial Analytics Section -->
+  <div class="financial-analytics-section">
+    <h2>Financial Analytics</h2>
+    <!-- Content such as tables and charts -->
+  </div>
+
+</div>
+
+<script>
+
+</script>
+
+</body>
+</html>
