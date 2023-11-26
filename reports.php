b652dc90bc866ff14115c2d6bb46b0260be6bd07
Ispraggins1 <143848081+Ispraggins1@users.noreply.github.com>
2023-11-26 18:18:15 -0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Valencia Medical - Reports & Analytics</title>
-  <link rel="stylesheet" href="styles7.css">
+  <link rel="stylesheet" href="reports.css">
 </head>
 <body>
 
